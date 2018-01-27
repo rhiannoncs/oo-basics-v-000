7b273885ca45b104643941e05b16b66490503a30
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-27 14:31:21 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,12 +8,20 @@ class Book
     @title
   end
   
-  def author= (author)
+  def author=(author)
     @author = author
   end
   
   def author
     @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
   end
   
 end
